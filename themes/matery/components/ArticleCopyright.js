afd4da5d451550fbed52bb6dc9a16ccb57287578
Shawn <74861650+hishawnchang@users.noreply.github.com>
2026-05-29 18:33:39 +0800

    Update ArticleCopyright.js

--- themes/matery/components/ArticleCopyright.js
+++ themes/matery/components/ArticleCopyright.js
@@ -21,7 +21,7 @@ export default function ArticleCopyright({ post }) {
 
   return (
     <section className='dark:text-gray-300 mt-6 mx-1 '>
-      <ul className='overflow-x-auto whitespace-nowrap text-sm dark:bg-gray-900 bg-gray-100 p-5 leading-8 border-l-2 border-indigo-500'>
+      <ul className='overflow-x-auto whitespace-nowrap text-sm dark:bg-gray-900 bg-gray-100 p-5 leading-8 border-l-2 border-blue-dark'>
         <li>
           <strong className='mr-2'>{locale.COMMON.AUTHOR}:</strong>
           <SmartLink href={'/about'} className='hover:underline'>
