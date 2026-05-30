156f19bd2f9a6963365f2be892650ce301939295
Shawn <74861650+hishawnchang@users.noreply.github.com>
2026-05-30 13:01:36 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,3 +1,60 @@
+// START ************网站字体*****************
+// ['font-serif','font-sans'] 两种可选，分别是衬线和无衬线: 参考 https://www.jianshu.com/p/55e410bd2115
+// 后面空格隔开的 font-light 是字体粗细，留空是默认粗细；参考 https://www.tailwindcss.cn/docs/font-weight
+FONT_STYLE: process.env.NEXT_PUBLIC_FONT_STYLE || 'font-serif font-normal',
+
+// 字体CSS 例如 https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css
+// 如果需要引入第三方字体，可以在此添加URL
+FONT_URL: [
+  // 'https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css',
+  'https://fonts.googleapis.com/css?family=Bitter&display=swap',
+  'https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@300&display=swap',
+  'https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@300&display=swap'
+],
+
+// 无衬线字体 例如'"LXGW WenKai"' , 如果字体有包含空格，需要用 "双 引 号" 包起来。
+FONT_SANS: [
+  // '"LXGW WenKai"',
+  '"PingFang SC"',
+  '-apple-system',
+  'BlinkMacSystemFont',
+  '"Hiragino Sans GB"',
+  '"Microsoft YaHei"',
+  '"Segoe UI Emoji"',
+  '"Segoe UI Symbol"',
+  '"Segoe UI"',
+  '"Noto Sans SC"',
+  'HarmonyOS_Regular',
+  '"Helvetica Neue"',
+  'Helvetica',
+  '"Source Han Sans SC"',
+  'Arial',
+  'sans-serif',
+  '"Apple Color Emoji"'
+],
+
+
+// 衬线字体 例如'"LXGW WenKai"'
+FONT_SERIF: [
+  // '"LXGW WenKai"',
+  'Bitter',
+  '"Noto Serif SC"',
+  'SimSun',
+  '"Times New Roman"',
+  'Times',
+  'serif',
+  '"Segoe UI Emoji"',
+  '"Segoe UI Symbol"',
+  '"Apple Color Emoji"'
+],
+FONT_AWESOME: process.env.NEXT_PUBLIC_FONT_AWESOME_PATH || 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css', // font-awesome 字体图标地址; 可选 /css/all.min.css ， https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/font-awesome/6.0.0/css/all.min.css
+
+// END ************网站字体*****************
+
+
+
+
+
 // 注: process.env.XX是Vercel的环境变量，配置方式见：https://docs.tangly1024.com/article/how-to-config-notion-next#c4768010ae7d44609b744e79e2f9959a
 
 const BLOG = {
